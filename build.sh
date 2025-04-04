a48547b77b0c131009de6b81fc8cbe01a801ef3a
balachandrankk <127740036+balachandrankk@users.noreply.github.com>
2025-04-04 11:46:18 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 docker build -t final .
 echo abc
 docker login -u balachandran2005 -p 19-May-05
-docker tag final balachandran2005/devops
-docker push balachandran2005/devops
+docker tag final balachandran2005/devo
+docker push balachandran2005/devo
 kubectl apply -f  deploy.yaml --validate=false
 kubectl apply -f svc.yaml --validate=false
